afce363be515318df6a7972e8f56070e8fe3ef34
Matthew Chertudi <mchertudi@auvik.com>
2022-07-16 00:01:13 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h2>Heyo, we're gonna do a React</h2>
+    </div>
+  )
+}
+
+export default App
