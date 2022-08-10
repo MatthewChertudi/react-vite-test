76e69f80c07270b2bb6305013cf9ab37f663bd96
Matthew Chertudi <mchertudi@auvik.com>
2022-08-10 16:02:50 -0600

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+// import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <h2>Heyo, we're gonna do a React</h2>
+      <ExpenseItem />
     </div>
   )
 }
